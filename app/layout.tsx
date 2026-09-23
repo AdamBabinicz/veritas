e468a9ca84e296a24e95dd10b9cc2d5bb6043c44
babinicz <babinicz@onet.pl>
2026-09-23 14:09:48 +0200

    feat(telemetry): verify and finalize dual Google Analytics G-5B1P108Z2D and GTM-PNC3NGF9 telemetry pipeline

--- app/layout.tsx
+++ app/layout.tsx
@@ -209,7 +209,6 @@ export default function RootLayout({
             src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
             height="0"
             width="0"
-            style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
 
