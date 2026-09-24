e5268bbb191938ad373ab1e56f58bbf7609ae0be
babinicz <babinicz@onet.pl>
2026-09-24 13:56:05 +0200

    perf(core-web-vitals): defer GTM loading to idle to restore mobile LCP score to 96+

--- app/layout.tsx
+++ app/layout.tsx
@@ -161,7 +161,23 @@ const jsonLd = {
   ],
 };
 
-const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`;
+// Czysty, bezbłędny dla audytorów (brak '{' w inline) i nieblokujący LCP skrypt ładowany w idle
+const gtmLoaderScript = `
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+  function initGTM() {
+    var f = document.getElementsByTagName('script')[0];
+    var j = document.createElement('script');
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=${GTM_ID}';
+    f.parentNode.insertBefore(j, f);
+  }
+  if ('requestIdleCallback' in window) {
+    window.requestIdleCallback(initGTM, { timeout: 3000 });
+  } else {
+    window.addEventListener('load', function() { setTimeout(initGTM, 1500); });
+  }
+`;
 
 export default function RootLayout({
   children,
@@ -191,11 +207,11 @@ export default function RootLayout({
 
         {children}
 
-        {/* Prawidłowo zserializowany skrypt GTM bez błędów składniowych w audytorach */}
+        {/* Ładowanie w bezczynności (idle) – nie blokuje renderowania LCP i nie psuje wyniku Mobile */}
         <Script
           id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{ __html: gtmScript }}
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{ __html: gtmLoaderScript }}
         />
       </body>
     </html>
